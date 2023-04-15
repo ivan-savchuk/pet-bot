9539273a14097c69b076c50e2900655912b40ce3
Ivan Savchuk <31822421+ivan-savchuk@users.noreply.github.com>
2023-04-15 22:50:04 +0300

    Setup linter in CI (#5)
    * Setup linter in CI
    
    * ci update
    
    * ci update
    
    * ci update
    
    * ci update
    
    * ci update
    
    * ci update

--- cmd/main/main.go
+++ cmd/main/main.go
@@ -2,12 +2,11 @@ package main
 
 import (
 	"fmt"
-	"log"
 	"os"
 	"time"
 
 	// tgbotapi "github.com/go-telegram-bot-api/telegram-bot-api/v5"
-	"github.com/ivan-savchuk/pet-etl/pkg/parsers"
+
 	"github.com/ivan-savchuk/pet-etl/pkg/pgdb"
 )
 
@@ -66,34 +65,34 @@ func main() {
 	// }
 }
 
-func getWeather() {
-	if WeatherAPIKey == "undefined" {
-		log.Fatalln("'OPEN_WEATHER_API_KEY' was not defined as environment variable.")
-		return
-	}
-	// Get data for Kyiv
-	wth, err := parsers.ParseWeather(50.45, 30.52, &WeatherAPIKey)
-	if err != nil {
-		log.Fatalf("Response from OpenWeatherMap was not obtained: %v", err)
-		return
-	}
-	if wth.Cod != 200 {
-		log.Fatalf("Response from OpenWeatherMap was not obtained, response status: %v\n", wth.Cod)
-		return
-	}
-	// Get data for Kyiv
-	aqi, err := parsers.ParseAQI(50.45, 30.52, &WeatherAPIKey)
-	if err != nil {
-		log.Fatalf("Response from OpenWeatherMap was not obtained: %v", err)
-		return
-	}
+// func getWeather() {
+// 	if WeatherAPIKey == "undefined" {
+// 		log.Fatalln("'OPEN_WEATHER_API_KEY' was not defined as environment variable.")
+// 		return
+// 	}
+// 	// Get data for Kyiv
+// 	wth, err := parsers.ParseWeather(50.45, 30.52, &WeatherAPIKey)
+// 	if err != nil {
+// 		log.Fatalf("Response from OpenWeatherMap was not obtained: %v", err)
+// 		return
+// 	}
+// 	if wth.Cod != 200 {
+// 		log.Fatalf("Response from OpenWeatherMap was not obtained, response status: %v\n", wth.Cod)
+// 		return
+// 	}
+// 	// Get data for Kyiv
+// 	aqi, err := parsers.ParseAQI(50.45, 30.52, &WeatherAPIKey)
+// 	if err != nil {
+// 		log.Fatalf("Response from OpenWeatherMap was not obtained: %v", err)
+// 		return
+// 	}
 
-	fmt.Printf("OpenWeatherMap Response status: %v\n", wth.Cod)
-	fmt.Printf("Current Weather in Kyiv: %v\n", wth.Weather[0].Description)
-	fmt.Printf("Current temparature: %v\n", fmt.Sprintf("%.1f", wth.Main.Temp))
-	fmt.Printf("Feels like: %v\n", fmt.Sprintf("%.1f", wth.Main.FeelsLike))
-	fmt.Printf("Humidity: %v\n", wth.Main.Humidity)
-	fmt.Printf("Pressure: %v\n", wth.Main.Pressure)
-	fmt.Printf("Air pollution in Kyiv (AQI): %v\n", aqi.List[0].Main.Aqi)
-	fmt.Println("Where 1 = Good, 2 = Fair, 3 = Moderate, 4 = Poor, 5 = Very Poor.")
-}
+// 	fmt.Printf("OpenWeatherMap Response status: %v\n", wth.Cod)
+// 	fmt.Printf("Current Weather in Kyiv: %v\n", wth.Weather[0].Description)
+// 	fmt.Printf("Current temparature: %v\n", fmt.Sprintf("%.1f", wth.Main.Temp))
+// 	fmt.Printf("Feels like: %v\n", fmt.Sprintf("%.1f", wth.Main.FeelsLike))
+// 	fmt.Printf("Humidity: %v\n", wth.Main.Humidity)
+// 	fmt.Printf("Pressure: %v\n", wth.Main.Pressure)
+// 	fmt.Printf("Air pollution in Kyiv (AQI): %v\n", aqi.List[0].Main.Aqi)
+// 	fmt.Println("Where 1 = Good, 2 = Fair, 3 = Moderate, 4 = Poor, 5 = Very Poor.")
+// }
